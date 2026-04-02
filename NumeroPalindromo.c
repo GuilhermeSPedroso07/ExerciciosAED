ef091a5b913156439c35b84b43f7b54bac678f05
Guilherme Pedroso <122842727+GuilhermeSPedroso07@users.noreply.github.com>
2026-04-02 00:16:23 -0300

    Add files via upload

---
+++ NumeroPalindromo.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int contarDigitos ( int x ) {
+    /*
+===============================================================================
+contarDigitos 
+
+    Essa função conta a quantidade de dígitos de uma int qualquer, considerando
+    o símbolo de negativo, e devolve para transformar em uma string, adicionando
+    1 para o '\0'
+===============================================================================
+*/
+
+    int count = 1;
+    unsigned int intTemp = x;
+
+    if ( x < 0 ) {
+        count = count + 1;
+    }
+
+    if ( x == 0 ) {
+        return 1;
+    }
+
+    while ( intTemp > 0 ) {
+        count = count +1;
+        intTemp = intTemp / 10;
+    }
+
+    return count;
+}
+
+bool isPalindrome ( int x ) {
+    /*
+===============================================================================
+isPalindrome
+
+    Essa função recebe uma int e verifica se o número recebido é um palíndromo
+===============================================================================
+*/
+    int numCar = contarDigitos ( x );
+    char charTemp;
+    char *numerosChar = ( char * ) malloc( sizeof( char ) * numCar );
+    snprintf ( numerosChar, numCar, "%d", x );
+
+    char *numerosInv = ( char * ) malloc ( sizeof ( char ) * numCar );
+    strcpy ( numerosInv, numerosChar );
+    int count1 = 0, count2 = strlen(numerosInv) - 1;
+
+    while ( count1 < count2 ) {
+        charTemp = numerosInv[count1];
+        numerosInv[count1++] = numerosInv[count2];
+        numerosInv[count2--] = charTemp;
+    }
+
+    if ( strcmp ( numerosInv, numerosChar ) == 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
