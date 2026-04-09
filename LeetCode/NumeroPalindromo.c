25683b95a28d0465396c21b371b8a869e48b5320
Guilherme Pedroso <122842727+GuilhermeSPedroso07@users.noreply.github.com>
2026-04-08 22:41:52 -0300

    Update NumeroPalindromo.c

--- LeetCode/NumeroPalindromo.c
+++ LeetCode/NumeroPalindromo.c
@@ -1,69 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-int contarDigitos ( int x ) {
-    int count = 1;
-    unsigned int intTemp = x;
-
+bool isPalindrome(int x) {
     if ( x < 0 ) {
-        count = count + 1;
-    }
-
-    if ( x == 0 ) {
-        return 1;
-    }
-
-    while ( intTemp > 0 ) {
-        count = count +1;
-        intTemp = intTemp / 10;
-    }
-
-    return count;
-}
-
-bool isPalindrome ( int x ) {
-
-    int numCar = contarDigitos ( x );
-    char charTemp;
-    char *numerosChar = ( char * ) malloc( sizeof( char ) * numCar );
-    snprintf ( numerosChar, numCar, "%d", x );
-
-    char *numerosInv = ( char * ) malloc ( sizeof ( char ) * numCar );
-    strcpy ( numerosInv, numerosChar );
-    int count1 = 0, count2 = strlen(numerosInv) - 1;
-
-    while ( count1 < count2 ) {
-        charTemp = numerosInv[count1];
-        numerosInv[count1++] = numerosInv[count2];
-        numerosInv[count2--] = charTemp;
-    }
-
-    if ( strcmp ( numerosInv, numerosChar ) == 0 ) {
-        free ( numerosChar );
-        free ( numerosInv );
-
-        return true;
-    } else {
-        free ( numerosChar );
-        free ( numerosInv );
-        
         return false;
     }
-}
+    int invertida = 0, digito, temp = x;
+    while ( temp > 0 ) {
+        digito = temp % 10;
+        if ( INT_MAX / 10 < invertida ) {
+            return false;
+        }
+        if ( INT_MAX / 10 == invertida && digito > 7 ) {
+            return false;
+        }
 
-int main () {
-    int x;
-
-    printf ( "Digite um número inteiro: " );
-    scanf ( "%d", &x );
-
-    if ( isPalindrome ( x ) ) {
-        printf ( "O número %d é um palíndromo.\n", x );
-    } else {
-        printf ( "O número %d não é um palíndromo.\n", x );
+        invertida *= 10;
+        invertida += digito;
+        temp /= 10;
     }
-
-    return 0;
+    if ( invertida == x ) {
+        return true;
+    } else {
+        return false;
+    }
 }
