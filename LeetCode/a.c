cdbf61209f4f56df555c4f35183865255a8c48b7
Guilherme Pedroso <122842727+GuilhermeSPedroso07@users.noreply.github.com>
2026-05-22 21:16:30 -0300

    Update a.c

--- LeetCode/a.c
+++ LeetCode/a.c
@@ -1,34 +1,47 @@
+#include <stdio.h>
+
 typedef struct list {
     int value;
     int manyTimes;
 
     struct list *next;
-} list;
+} lista;
 
-void addToList ( int num, list **list ) {
-    while ( ( *list ) ->value != num ) {
-        if ( ( *list ) ->next == NULL ) {
-            ( *list ) ->next = malloc ( sizeof ( list ) );
-            ( *list ) ->next->value = num;
+void addToList ( int num, lista **list ) {
+    lista *ptr = ( *list );
+    while ( ptr->value != num ) {
+        if ( ptr->next == NULL ) {
+            ptr->next = malloc ( sizeof ( list ) );
+            ptr->next->value = num;
         }
-        ( *list ) = ( *list ) ->next;
+        ptr = ptr->next;
     }
-    ( *list ) ->manyTimes++;
+    ptr->manyTimes++;
 }
 
-int majorElement ( list *list, int n ) {
-    while ( list->manyTimes < n/2 ) {
+int majorElement ( lista *list, int n ) {
+    while ( list->manyTimes < ( n/2 ) ) {
         list = list->next;
     }
     return list->value;
 }
 
 int majorityElement(int* nums, int numsSize) {
-    list *list = malloc ( sizeof ( list ) );
+    lista *list = malloc ( sizeof ( list ) );
     list->value = nums[0];
     list->manyTimes = 1;
     for ( int count = 1; count < numsSize; count++ ) {
         addToList ( nums[count], &list );
     }
     return majorElement ( list, numsSize );
+}
+
+int main(int argc, char const *argv[])
+{
+    int nums[5] = {3,4,4,4,2};
+    int numsSize = 5;
+
+    printf("%d\n", majorityElement(nums, numsSize));
+
+    return 0;
 }
