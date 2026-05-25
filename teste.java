ed55f0e81a33be24fe804680f05e63b0be07faf4
Guilherme Pedroso <122842727+GuilhermeSPedroso07@users.noreply.github.com>
2026-05-25 02:04:33 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,3 +1,41 @@
+public class Animal {
+    private int fome;
+    private int sono;
+    private int diasDeVida;
+    
+    public void Animal () {
+        fome = 0;
+        sono = 0;
+        diasDeVida = 0;
+    }
+    
+    abstract void fazerBarulho();
+    
+    public void comer () {
+        fome--;
+        sono++;
+        
+        if ( fome < 0 ) {
+            fome = 0;
+        }
+    }
+    
+    public void dormir () {
+        diasDeVida++;
+        sono -= 5;
+        
+        if ( sono < 0 ) {
+            sono = 0;
+        }
+    }
+    
+    public void vaguear () {
+        fome++;
+        sono++;
+    }
+    
+}
+
 public class Leao extends Animal {
     public void fazerBarulho () {
         System.out.print("Roar");
@@ -13,4 +51,9 @@ public class Leao extends Animal {
         System.out.print("" + this.sono);
         System.out.print("" + this.diasDeVida);
     }
+}
+
+public class Main
+{
+	Animal novoAnimal = new Leao();
 }
