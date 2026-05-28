e81184ef67c5e83b1ddd131bc6e854b378debfd5
Guilherme Pedroso <122842727+GuilhermeSPedroso07@users.noreply.github.com>
2026-05-28 12:11:54 -0300

    Add files via upload

---
+++ LeetCode/LongestCommomPrefix/src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if ( strsSize == 0 ) {
+        return "";
+    }
+    int size = strlen ( strs[0] );
+    char *prefix = ( char * ) malloc ( sizeof ( char ) * (size + 1 ));
+    strcpy ( prefix, strs[0] );
+    for ( int i = 1; i < strsSize; i++ ) {
+        printf ( "%s", prefix);
+        for ( int j = size; j >= 0; j-- ) {
+            if ( strncmp ( prefix, strs[i], j ) == 0 ) {
+                prefix[j] = '\0';
+                size = j;
+                break;
+            } else if ( j == 1 ) {
+                return "";
+            }
+        }
+    }
+    return prefix;
+}
+
+int main(int argc, char const *argv[])
+{
+    char *strs[3];
+    char string1[7];
+    char string2[5];
+    char string3[7];
+    strcpy ( string1, "flower" );
+    strcpy ( string2, "flow" );
+    strcpy ( string3, "flight" );
+    strs[0] = string1;
+    strs[1] = string2;
+    strs[2] = string3;
+
+
+    printf ( "%s", longestCommonPrefix(strs, 3 ));
+
+    return 0;
+}
